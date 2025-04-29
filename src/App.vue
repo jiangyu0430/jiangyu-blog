2eda24145ebb7e7db9c53b0a8e0c0bbe83783be0
jiangyua <1119274263@qq.com>
2025-04-29 16:31:47 +0800

    更新页面内容

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         <h2 class="text-xl font-semibold mb-4">欢迎来到我的博客</h2>
         <p class="text-gray-700 leading-relaxed">
           这是一个使用 Vue 3 + Tailwind CSS 构建的简单博客页面模板。
-          你可以在这里修改内容，添加组件，实现你自己的博客功能。
+          你可以在这里修改的大爹内容，添加组件，实现你自己的博客功能。
         </p>
       </section>
     </main>
